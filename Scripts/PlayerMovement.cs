398f7286b894004791bd444c84cf7448c40e3bc4
aknez615 <al683193@ucf.edu>
2025-03-04 14:29:18 -0500

    Update PlayerMovement.cs

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public static PlayerMovement instance;
 
+    public GameManager gameManager; //GameManager reference
+
     public float walkSpeed = 3.0f; //Walk speed to be slower than monster
     public float runSpeed = 6.0f; //Run speed to be faster than monster
     public float stamina = 100f; //Max stamina. Will be changed later to make it easier or harder
@@ -16,6 +18,8 @@ public class PlayerMovement : MonoBehaviour
     public float mouseSensitivity = 2f; //How senstive the camera mouse movement will be
     float cameraVerticalRotation = 0f; //Setting up how the variable will handle movement
     float cameraHorizontalRotation = 0f; //Setting up for the horizontal movement
+    public float interactionRange = 3f; //Distance for the player to interact with an object (ex: door)
+    public LayerMask interactableLayer; //To detect objects that are interactable
 
     bool lockedCursor = true;
 
@@ -60,16 +64,26 @@ public class PlayerMovement : MonoBehaviour
         float inputX = Input.GetAxis("Mouse X") * mouseSensitivity; //Looking left and right
         float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity; //Looking up and down
 
-        //Rotating the camera around the X axis
-        cameraVerticalRotation -= inputY;
-        cameraHorizontalRotation -= inputX;
-        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
-        cameraHorizontalRotation = Mathf.Clamp(cameraHorizontalRotation, -90f, 90f);
-        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
-        transform.localEulerAngles = Vector3.up * cameraHorizontalRotation;
+        //Xbox thumbstick inputs
+        /* float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        //Joystick input for Xbox
+        Vector3 move = new Vector3(moveX, 0, moveY);
+        transform.Translate(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            Interact();
+        } */
 
         //Rotating the player object and the camera around the Y axis
         player.Rotate(Vector3.up * inputX);
+
+        //Rotating the camera around the X axis
+        cameraVerticalRotation -= inputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
+        Camera.main.transform.localRotation = Quaternion.Euler(cameraVerticalRotation, 0f, 0f); //Camera's vertical rotation
 
         isRunning = Input.GetKey(KeyCode.LeftShift) && currentStamina > staminaThreshold;
 
@@ -83,7 +97,44 @@ public class PlayerMovement : MonoBehaviour
             MoveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized * walkSpeed;
         }
     }
+    /*
+    void Interact()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, interactionRange, interactableLayer))
+        {
+            if (hit.collider.CompareTag("Door"))
+            {
+                Debug.Log("Interacting with the door. Change scene.");
+                ChangeScene();
+            }
 
+            else if (hit.collider.CompareTag("Item"))
+            {
+                Debug.Log("Picking up item.");
+                PickUpItem(hit.collider.gameObject);
+            }
+        }
+    }
+    
+    void ChangeScene()
+    {
+        if (gameManager != null)
+        {
+            gameManager.ChangeScene(""); //Scene name to change it to
+        }
+
+        else
+        {
+            Debug.LogError("GameManager reference is missing!");
+        }
+    }
+    
+    void PickUpItem(GameObject item)
+    {
+        Destroy(item);
+    }
+    */
     private void MovePlayer()
     {
         Vector3 movement = MoveDirection * Time.fixedDeltaTime;
