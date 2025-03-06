1bf3898eec0ae31100455860d8641e26dc516892
MangoDraco <le303029@ucf.edu>
2025-03-06 17:52:28 -0500

    UIAdded
    Another 'is what it says on the tin'. UI added.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -33,14 +33,11 @@ public class PlayerMovement : MonoBehaviour
 
     //variables i'm using for stare of death
     public GameObject enemy;
-    public GameObject key;
     public LayerMask targetMask; //will look for Enemy layer :3
     public LayerMask obstructionMask;
     private float sightRadius = 10.0f;
     private float sightAngle = 20.0f;
     private bool enemyDetected;
-    private bool pickupRange;
-    public bool hasKey;
     Enemy enemyS;
 
     private void Start()
@@ -59,17 +56,6 @@ public class PlayerMovement : MonoBehaviour
         HandleStamina();
         UpdateStaminaUI();
         CheckForStaring();
-        if (pickupRange)
-        {
-            if (Input.GetKey(KeyCode.E))
-            {
-                hasKey = true;
-                if (key != null)
-                {
-                    Destroy(key);
-                }
-            }
-        }
     }
 
     private void FixedUpdate()
@@ -189,15 +175,6 @@ public class PlayerMovement : MonoBehaviour
             Destroy(this.transform.gameObject);
             GameManager.isGameOver = true;
             GameManager.GameOver();
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "key")
-        {
-            Debug.Log("Can Pickup");
-            pickupRange = true;
         }
     }
 }
