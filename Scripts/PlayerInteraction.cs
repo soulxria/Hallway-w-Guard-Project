cda59a5c4a439ea969f3b510476753ccf17c343b
aknez615 <al683193@ucf.edu>
2025-03-10 22:31:31 -0400

    Alyssa 10:31pm 3/10/2025

--- Scripts/PlayerInteraction.cs
+++ Scripts/PlayerInteraction.cs
@@ -5,21 +5,63 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour
 {
     public float interactionRange = 3f;
-    public Camera playerCamera;
+    public LayerMask interactableLayer;
+    public Transform playerCamera;
+
+    private PlayerMovement playerMovement;
+
+    private void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>(); //So PlayerMovement and PlayerInteraction are on same GameObject
+    }
 
     private void Update()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, interactionRange))
+        HandleInteraction();
+        InteractWithObject();
+    }
+
+    private void HandleInteraction()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            Interaction interaction = hit.collider.GetComponent<Interaction>();
-            if (interaction != null)
+            RaycastHit hit;
+            if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, interactionRange, interactableLayer))
             {
-                if (Input.GetKeyDown(KeyCode.E)) //Interact with E key
-                {
-                    interaction.Interact();
-                }
+                InteractWithObject(hit.collider);
             }
+        }
+    }
+
+    private void InteractWithObject(Collider collider)
+    {
+        if (collider.CompareTag("Key"))
+        {
+            string keyName = collider.gameObject.name;
+            playerMovement.SetKey(keyName);
+            Destroy(collider.gameObject);
+        }
+
+        else if (collider.CompareTag("Door"))
+        {
+            string doorName = collider.gameObject.name;
+            UnlockDoor(collider, doorName);
+        }
+    }
+
+    private void UnlockDoor(Collider doorCollider, string doorName)
+    {
+        string requiredKey = doorName + "Key";
+
+        if (playerMovement.HasKey(requiredKey))
+        {
+            Debug.Log($"Unlocked the {doorName} with the {requiredKey}");
+            doorCollider.gameObject.GetComponent<Collider>().enabled = false;
+        }
+
+        else
+        {
+            Debug.Log($"You need the {requiredKey} to unlock this door");
         }
     }
 }
